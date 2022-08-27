1f45b16474ca41e045310c1c26d169891cbdb674
Osain002 <103956419+Osain002@users.noreply.github.com>
2022-08-28 00:30:51 +0100

    init commit

--- server/server.js
+++ server/server.js
@@ -1,26 +1,20 @@
 const express = require('express')
 const mysql = require('mysql2')
 const cors = require('cors')
-const bodyParser = require('body-parser')
-
+const {query, body, validationResult} = require('express-validator')
 const app = express()
 
 app.use(cors())
-
-//app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
-
-
+const today = new Date().toISOString().slice(0, 10);
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
     password: "1999MySqlOtis",
     database: 'Restaur'
 })
-
-
 db.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
@@ -28,75 +22,82 @@ db.connect(function (err) {
 
 
 
+const query_and_send = (qstring, res) => {
+    db.query(qstring, (err, result) => {
+        if (err) {
+            return res.status(400).json(err)
+        } else {
+            return res.status(200).json(result)
+        }
+    })
+}
 
-app.get('/checkAvailability', (req, res) => {
 
-    const partySize = req.query.partySize
-    const date = req.query.date
-    const CheckInTime = req.query.CheckInTime
-    const CheckOutTime = req.query.CheckOutTime
 
-    const query = `SELECT TableID FROM TABLES R
+app.get('/retrieveBooking', (req,res) => {
+    const id = req.query.id
+    const lName = req.query.lName
+    const query = `SELECT * FROM BOOKINGS WHERE BookingID = ${id} AND LastName="${lName}"` 
+    
+    query_and_send(query, res)
+})
+
+
+
+
+
+
+app.get('/checkAvailability', 
+            query('Mobile').isMobilePhone(),
+            query('date').isAfter(today),
+
+            (req, res) => {
+    
+    const {partySize, date, CheckInTime, CheckOutTime} = req.query
+    const errors = validationResult(req);
+
+    const query = `
+    SELECT TableID FROM TABLES R
     WHERE capacity > ${partySize} AND NOT EXISTS 
     (SELECT * FROM BOOKINGS 
      WHERE TableID = R.TableID
-        AND cDate = '${date}'
+        AND Date = '${date}'
         AND CheckOutTime BETWEEN '${CheckInTime}' and '${CheckOutTime}'
-    )
-    `
+    )`
 
-    db.query(query, (err, result) => {
-
-        if (err) {
-            res.status(400).json(err)
-        } else {
-            res.status(200).json(result)
-        }
-    })
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    query_and_send(query, res)
 })
+
+
+
+
 
 
 
 app.post('/addBooking', (req, res) => { //slots 0900-1100, 11-1300, 1300-1500, 1500-1700
+    const {FirstName, LastName, PartySize, Date, From, To, Mobile, table} = req.body
 
-
-    const fname = req.body.fName
-    const lname = req.body.lName
-    const partySize = req.body.party
-    const date = req.body.date
-    const CheckInTime = req.body.from
-    const CheckOutTime = req.body.to
-    const contact = req.body.contact
-    const idTables = req.body.table
-
-
-    const insert = `INSERT INTO BOOKINGS (TableID, Guests, cDate, CheckInTime, CheckOutTime, fName, lName, Contact)
-                    VALUES ('${idTables}', '${partySize}', '${date}' ,'${CheckInTime}', '${CheckOutTime}', '${fname}', '${lname}', '${contact}')
+    const insert = `INSERT INTO BOOKINGS (TableID, Guests, Date, CheckInTime, CheckOutTime, FirstName, LastName, Contact)
+                    VALUES ('${table}', '${PartySize}', '${Date}' ,'${From}', '${To}', '${FirstName}', '${LastName}', '${Mobile}')
                     `
+   query_and_send(insert, res)
+})
 
-    //WHERE NOT EXISTS (SELECT * FROM BOOKINGS WHERE cDate='${date}' AND Contact = '${contact}')
 
-    db.query(insert, (err, result) => {
-        if (err) {
-            res.status(400).json(err)
-        } else {
-            res.status(200).json(result)
-        }
-    })
+app.delete('/deleteBooking', (req,res) => {
+    const id = req.body.id
+    const del = `DELETE FROM BOOKINGS WHERE BookingID = ${id}`
+    query_and_send(del, res)
 })
 
 
 
-
-app.post('/test', (req,res)=>{
-    console.log("d",req.body.fName)
-    res.status(200).json(req.body)
-
-})
-
-
-app.listen("8080", () => {
-    console.log('server running on port 8080')
+const port = "8080"
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
 
 
