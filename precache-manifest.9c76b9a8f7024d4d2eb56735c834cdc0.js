39f4240d0ee7271404bbcd188db1a31846a014e0
Osain002 <103956419+Osain002@users.noreply.github.com>
2022-08-30 01:02:48 +0100

    Updates

---
+++ precache-manifest.9c76b9a8f7024d4d2eb56735c834cdc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c2f40c9fb9ad29591b6",
+    "url": "/le-restaurant/static/css/main.2623f451.chunk.css"
+  },
+  {
+    "revision": "5c2f40c9fb9ad29591b6",
+    "url": "/le-restaurant/static/js/main.5c2f40c9.chunk.js"
+  },
+  {
+    "revision": "eada04647b7149b17abb",
+    "url": "/le-restaurant/static/js/1.eada0464.chunk.js"
+  },
+  {
+    "revision": "6854d8cea14087d50d98",
+    "url": "/le-restaurant/static/js/runtime~main.6854d8ce.js"
+  },
+  {
+    "revision": "8183ba8fd6a9f883de2156dd8ae0de29",
+    "url": "/le-restaurant/index.html"
+  }
+];
